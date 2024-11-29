306c4127780df355afdb7cdd71bc629825f027cb
Kevin Lim <zzkevinlim@gmail.com>
2024-11-29 18:09:23 +1300

    20241129180923

--- wp-content/themes/thecareeracademy/page.php
+++ wp-content/themes/thecareeracademy/page.php
@@ -3,7 +3,6 @@ get_header();
 ?>
 
 <main>
-	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sunt maxime nulla voluptatum at quia! Voluptate, hic. Perferendis, voluptatem neque! Optio voluptatum ut eos debitis, veniam neque consequuntur culpa omnis nam sed, autem fuga natus vero fugiat sequi beatae pariatur tempora qui nobis dicta ab necessitatibus. Odit earum iure quod necessitatibus, quo blanditiis esse mollitia eaque tempora provident. Ad fuga aperiam officia qui reprehenderit quae, cupiditate inventore quis doloribus? Ab vitae laboriosam quis accusantium? Culpa, voluptatem sed maxime doloremque voluptas quaerat iste voluptate impedit error eos enim nam et quam. Soluta laboriosam ipsa repellat doloribus rerum distinctio maiores illo unde?</div>
 	<?php
 	while (have_posts()) :
 		the_post();
