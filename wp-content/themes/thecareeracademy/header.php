9728002602675a078dc3414f23208d110362f78f
Kevin Lim <zzkevinlim@gmail.com>
2024-11-30 23:52:11 +1300

    20241130235211

--- wp-content/themes/thecareeracademy/header.php
+++ wp-content/themes/thecareeracademy/header.php
@@ -1,3 +1,60 @@
+<?php
+
+function menu_build_array($menu_items)
+{
+	$menu_array = [];
+
+	foreach ($menu_items as $menu_item) {
+		if ($menu_item->menu_item_parent) {
+			$menu_depth = 1;
+			$menu_array = menu_find_parent(
+				$menu_array,
+				$menu_item->menu_item_parent,
+				$menu_item,
+				$menu_depth
+			);
+		} else {
+			$menu_array[$menu_item->ID]['title'] = $menu_item->title;
+			$menu_array[$menu_item->ID]['url'] = $menu_item->url;
+			$menu_array[$menu_item->ID]['depth'] = 1;
+		}
+	}
+
+	return $menu_array;
+}
+
+function menu_find_parent($menu_array, $menu_parent_id, $menu_current_item, $menu_depth)
+{
+	foreach ($menu_array as $menu_index => $menu_item) {
+		if (isset($menu_item['children'])) {
+			$menu_array[$menu_index]['children'] = menu_find_parent(
+				$menu_item['children'],
+				$menu_parent_id,
+				$menu_current_item,
+				$menu_depth + 1
+			);
+		}
+
+		if ($menu_index == $menu_parent_id) {
+			$menu_array[$menu_index]['children'][$menu_current_item->ID]['title'] = $menu_current_item->title;
+			$menu_array[$menu_index]['children'][$menu_current_item->ID]['url'] = $menu_current_item->url;
+			$menu_array[$menu_index]['children'][$menu_current_item->ID]['depth'] = $menu_depth + 1;
+
+			return $menu_array;
+		}
+	}
+
+	return $menu_array;
+}
+
+$menu_items = wp_get_nav_menu_items(get_nav_menu_locations()['header-menu']);
+
+$menu_array = menu_build_array($menu_items);
+
+dd($menu_array);
+
+?>
+
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
@@ -48,3 +105,77 @@
 			<?php wp_nav_menu(array('theme_location' => 'header-menu',)); ?>
 		</nav>
 	</header>
+
+
+
+	<div class="container mx-auto px-4">
+
+		<div x-data="{
+        menuBarOpen: false,
+        menuBarMenu: ''
+    }"
+			@click.away="menuBarOpen=false"
+			class="relative top-0 left-0 z-50 w-auto transition-all duration-150 ease-out">
+			<div class="relative top-0 left-0 z-40 w-auto h-10 transition duration-200 ease-out">
+				<div class="w-full h-full p-1 bg-white border rounded-md border-neutral-200/80">
+					<div class="flex justify-between w-full h-full select-none text-neutral-900">
+
+
+						<!-- Edit Button -->
+						<div class="relative h-full cursor-default">
+							<button @click="menuBarOpen=true; menuBarMenu='edit'" @mouseover="menuBarMenu='edit'" :class="{ 'bg-neutral-100' : menuBarOpen && menuBarMenu == 'edit'}" class="rounded text-sm cursor-default flex items-center leading-tight justify-center px-3 py-1.5 h-full hover:bg-neutral-100">
+								Edit
+							</button>
+							<div
+								x-show="menuBarOpen && menuBarMenu=='edit'"
+								x-transition:enter="transition ease-linear duration-100"
+								x-transition:enter-start="-translate-y-1 opacity-90"
+								x-transition:enter-end="translate-y-0 opacity-100"
+								class="absolute top-0 z-50 min-w-[8rem] text-neutral-800 rounded-md border border-neutral-200/70 bg-white mt-10 text-sm p-1 shadow-md w-48 -translate-x-0.5"
+								x-cloak>
+
+								<button @click="menuBarOpen=false" class="relative flex justify-between w-full cursor-default select-none group items-center rounded px-2 py-1.5 hover:bg-neutral-100 hover:text-neutral-900 outline-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none">
+									<span>Undo</span>
+									<span class="ml-auto text-xs tracking-widest text-neutral-400 group-hover:text-neutral-600">⌘Z</span>
+								</button>
+								<button @click="menuBarOpen=false" class="relative flex justify-between w-full cursor-default select-none group items-center rounded px-2 py-1.5 hover:bg-neutral-100 hover:text-neutral-900 outline-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none">
+									<span>Redo</span>
+									<span class="ml-auto text-xs tracking-widest text-neutral-400 group-hover:text-neutral-600">⇧⌘Z</span>
+								</button>
+								<div class="h-px my-1 -mx-1 bg-neutral-200"></div>
+								<button class="relative w-full group">
+									<div class="flex cursor-default select-none items-center rounded px-2 hover:bg-neutral-100 py-1.5 outline-none">
+										<span>Find</span>
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 ml-auto">
+											<polyline points="9 18 15 12 9 6"></polyline>
+										</svg>
+									</div>
+									<div data-submenu="" class="absolute top-0 right-0 invisible mr-1 duration-200 ease-out translate-x-full opacity-0 group-hover:mr-0 group-hover:visible group-hover:opacity-100">
+										<div class="z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white p-1 shadow-md animate-in slide-in-from-left-1 w-32">
+											<div @click="menuBarOpen=false" class="relative flex cursor-default select-none items-center rounded px-2 py-1.5 hover:bg-neutral-100 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50">Search the web</div>
+											<div class="h-px my-1 -mx-1 bg-neutral-200"></div>
+											<div @click="menuBarOpen=false" class="relative flex cursor-default select-none items-center rounded px-2 py-1.5 hover:bg-neutral-100 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50">Find...</div>
+											<div @click="menuBarOpen=false" class="relative flex cursor-default select-none items-center rounded px-2 py-1.5 hover:bg-neutral-100 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50">Find Next</div>
+											<div @click="menuBarOpen=false" class="relative flex cursor-default select-none items-center rounded px-2 py-1.5 hover:bg-neutral-100 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50">Find Previous</div>
+										</div>
+									</div>
+								</button>
+								<div class="h-px my-1 -mx-1 bg-neutral-200"></div>
+								<button @click="menuBarOpen=false" class="relative flex justify-between w-full cursor-default select-none group items-center rounded px-2 py-1.5 hover:bg-neutral-100 hover:text-neutral-900 outline-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none">
+									<span>Cut</span>
+								</button>
+								<button @click="menuBarOpen=false" class="relative flex justify-between w-full cursor-default select-none group items-center rounded px-2 py-1.5 hover:bg-neutral-100 hover:text-neutral-900 outline-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none">
+									<span>Copy</span>
+								</button>
+								<button @click="menuBarOpen=false" class="relative flex justify-between w-full cursor-default select-none group items-center rounded px-2 py-1.5 hover:bg-neutral-100 hover:text-neutral-900 outline-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none">
+									<span>Paste</span>
+								</button>
+							</div>
+						</div>
+						<!-- End Edit Button -->
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
--- wp-content/themes/thecareeracademy/tailwind.config.js
+++ wp-content/themes/thecareeracademy/tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
   important: true,
   content: ["./src/**/*.{js,scss}", "./*.php",],
