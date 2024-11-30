ffe6193a07e13b5bc6864b416b14235e1199cf15
Kevin Lim <zzkevinlim@gmail.com>
2024-11-30 20:32:15 +1300

    20241130203215

---
+++ wp-content/themes/storefront-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/includes/model-functions.php';
+require __DIR__ . '/includes/view-functions.php';
+require __DIR__ . '/includes/controller-functions.php';
+require __DIR__ . '/includes/helper-functions.php';
--- wp-content/themes/thecareeracademy/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require get_template_directory() . '/includes/model-functions.php';
-require get_template_directory() . '/includes/view-functions.php';
-require get_template_directory() . '/includes/controller-functions.php';
-require get_template_directory() . '/includes/helper-functions.php';
